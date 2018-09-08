b27e2060d37643a3d0fc4032fc66797a8027ee26
Guntram Blohm <gbl@guntram.de>
2018-09-08 12:11:54 +0200

    Update to MC 1.13

--- src/me/ScarleTomato/IronElevators/EventListener.java
+++ src/me/ScarleTomato/IronElevators/EventListener.java
@@ -11,66 +11,64 @@ import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class EventListener implements Listener{
 	
-	IronElevators inst;
+    IronElevators inst;
 
-	public EventListener(IronElevators inst){
-		this.inst = inst;
-	}
-	
-	@EventHandler
-	public void downElevator(PlayerToggleSneakEvent e){
-		Player p = e.getPlayer();
-		Block b = p.getLocation().getBlock().getRelative(BlockFace.DOWN);
-		if(p.hasPermission("ironelevators.use") && !p.isSneaking()
-				&& b.getType() == inst.elevatorMaterial){
-			b = b.getRelative(BlockFace.DOWN, inst.minElevation);
-			int i = inst.maxElevation; //16
-			while	(i>0 && !	(
-							   b.getType() == inst.elevatorMaterial
-							&& b.getRelative(BlockFace.UP).getType().isTransparent()
-							&& b.getRelative(BlockFace.UP, 2).getType().isTransparent()
-							)
-					)
-			{
-				//e.getPlayer().sendMessage("" + b.getLocation() + b.getType());
-				i--;
-				b = b.getRelative(BlockFace.DOWN);
-			}
-			if(i>0)
-			{
-				Location l = p.getLocation();
-				l.setY(l.getY()-inst.maxElevation-3+i);
-				p.teleport(l, inst.cause);
-				p.getWorld().playSound(l, inst.elevatorWhoosh, 1, 0);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void upElevator(PlayerMoveEvent e){
-		Player p = e.getPlayer();
-		Block b = e.getTo().getBlock().getRelative(BlockFace.DOWN);
-		if(p.hasPermission("ironelevators.use") && e.getFrom().getY() < e.getTo().getY()
-				&& b.getType() == inst.elevatorMaterial){
-			b = b.getRelative(BlockFace.UP, inst.minElevation);
-			int i = inst.maxElevation;
-			while(i>0 && !(
-							   b.getType() == inst.elevatorMaterial
-							&& b.getRelative(BlockFace.UP).getType().isTransparent()
-							&& b.getRelative(BlockFace.UP, 2).getType().isTransparent()
-						  )
-				 )
-			{
-				i--;
-				b = b.getRelative(BlockFace.UP);
-			}
-			if(i>0)
-			{
-				Location l = p.getLocation();
-				l.setY(l.getY()+inst.maxElevation+3-i);
-				p.teleport(l, inst.cause);
-				p.getWorld().playSound(l, inst.elevatorWhoosh, 1, 0);
-			}
-		}
-	}
+    public EventListener(IronElevators inst){
+        this.inst = inst;
+    }
+
+    @EventHandler
+    public void downElevator(PlayerToggleSneakEvent e){
+        Player p = e.getPlayer();
+        Block b = p.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if (p.hasPermission("ironelevators.use") && !p.isSneaking() && b.getType() == inst.elevatorMaterial) {
+            b = b.getRelative(BlockFace.DOWN, inst.minElevation);
+            int i = inst.maxElevation;
+            while (i>0 && ! (
+                    b.getType() == inst.elevatorMaterial
+                 && b.getRelative(BlockFace.UP).getType().isTransparent()
+                 && b.getRelative(BlockFace.UP, 2).getType().isTransparent()
+                )
+            )
+            {
+                i--;
+                b = b.getRelative(BlockFace.DOWN);
+            }
+            if (i>0)
+            {
+                Location l = p.getLocation();
+                l.setY(l.getY()-inst.maxElevation-3+i);
+                p.teleport(l, inst.cause);
+                p.getWorld().playSound(l, inst.elevatorWhoosh, 1, 0);
+            }
+        }
+    }
+
+    @EventHandler
+    public void upElevator(PlayerMoveEvent e){
+        Player p = e.getPlayer();
+        Block b = e.getTo().getBlock().getRelative(BlockFace.DOWN);
+        if (p.hasPermission("ironelevators.use") && e.getFrom().getY() < e.getTo().getY()
+                        && b.getType() == inst.elevatorMaterial) {
+            b = b.getRelative(BlockFace.UP, inst.minElevation);
+            int i = inst.maxElevation;
+            while(i>0 && !(
+                        b.getType() == inst.elevatorMaterial
+                     && b.getRelative(BlockFace.UP).getType().isTransparent()
+                     && b.getRelative(BlockFace.UP, 2).getType().isTransparent()
+               )
+            )
+            {
+                    i--;
+                    b = b.getRelative(BlockFace.UP);
+            }
+            if (i>0)
+            {
+                Location l = p.getLocation();
+                l.setY(l.getY()+inst.maxElevation+3-i);
+                p.teleport(l, inst.cause);
+                p.getWorld().playSound(l, inst.elevatorWhoosh, 1, 0);
+            }
+        }
+    }
 }
--- src/me/ScarleTomato/IronElevators/IronElevators.java
+++ src/me/ScarleTomato/IronElevators/IronElevators.java
@@ -2,87 +2,74 @@ package me.ScarleTomato.IronElevators;
 
 import java.awt.Color;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.IOException;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IronElevators extends JavaPlugin {
-	public static final int FILE_COPY_MAX_BYTE_SIZE = 4096;
-	
-	//default config values
-	public int maxElevation = 14, minElevation = 3;
-	public Material elevatorMaterial = Material.IRON_BLOCK;
-	public Sound elevatorWhoosh = Sound.ENTITY_IRON_GOLEM_ATTACK;
-        public TeleportCause cause = TeleportCause.UNKNOWN;
-	
-	EventListener listener;
-	FileConfiguration config;
-	File configFile;
-	
-        @Override
-	public void onEnable(){
-		//load config
-		loadConfig();
-		getConfigValues();
-		
-		//Register event listener
-		listener = new EventListener(this);
-		getServer().getPluginManager().registerEvents(this.listener, this);
-	}
-	
-	void getConfigValues(){
-		//integer values
-		maxElevation = config.getInt("maxElevation");
-		minElevation = config.getInt("minElevation");
-		elevatorMaterial = Material.valueOf(config.getString("elevatorMaterial"));
-		elevatorWhoosh = Sound.valueOf(config.getString("elevatorWhoosh"));
-                cause = TeleportCause.valueOf(config.getString("teleportcause", "UNKNOWN"));
-	}
-	
-	void loadConfig(){
-		try{
-			config = new YamlConfiguration();
-			configFile = new File(getDataFolder(), "config.yml");
-			if(!configFile.exists()){
-				configFile.getParentFile().mkdirs();
-				//copy(getResource("config.yml"), configFile);
-				
-				//build config from defaults
-				config.set("minElevation", minElevation);
-				config.set("maxElevation", maxElevation);
-				config.set("elevatorMaterial", elevatorMaterial.toString());
-				config.set("elevatorWhoosh", elevatorWhoosh.toString());
-                                config.set("teleportcause", TeleportCause.UNKNOWN.name());
-				config.save(configFile);
-			}
-			config.load(configFile);
-		}catch(Exception e){
-			Bukkit.getLogger().log(Level.WARNING, "{0}Exception {1}when loading configuration file.\n{2}", new Object[]{ChatColor.RED, Color.white, e.getMessage()});
-		}
-	}
-	
-	public static void copy(InputStream in, File file){
-		try {
-			OutputStream out = new FileOutputStream(file);
-			byte[] buf = new byte[FILE_COPY_MAX_BYTE_SIZE];
-			int len;
-			while((len=in.read(buf))>0){
-				out.write(buf,0,len);
-			}
-			out.close();
-			in.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+    //default config values
+    public int maxElevation = 14, minElevation = 3;
+    public Material elevatorMaterial = Material.IRON_BLOCK;
+    public Sound elevatorWhoosh = Sound.ENTITY_IRON_GOLEM_ATTACK;
+    public TeleportCause cause = TeleportCause.UNKNOWN;
+
+    EventListener listener;
+    FileConfiguration config;
+    File configFile;
+
+    @Override
+    public void onEnable() {
+        loadConfig();
+        getConfigValues();
+        listener = new EventListener(this);
+        getServer().getPluginManager().registerEvents(this.listener, this);
+    }
+
+    void getConfigValues() {
+        maxElevation = config.getInt("maxElevation");
+        minElevation = config.getInt("minElevation");
+        try {
+            elevatorMaterial = Material.valueOf(config.getString("elevatorMaterial").toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            this.getLogger().log(Level.WARNING, "Elevator Material not found: {0}, using iron block instead", ex.getMessage());
+            elevatorMaterial = Material.IRON_BLOCK;
+        }
+        try {
+            elevatorWhoosh = Sound.valueOf(config.getString("elevatorWhoosh").toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            this.getLogger().log(Level.WARNING, "Elevator whoosh sound not found: {0}, using iron golem instead", ex.getMessage());
+            elevatorWhoosh = Sound.ENTITY_IRON_GOLEM_ATTACK;
+        }
+        cause = TeleportCause.valueOf(config.getString("teleportcause", "UNKNOWN"));
+    }
+
+    void loadConfig() {
+        try {
+            config = new YamlConfiguration();
+            configFile = new File(getDataFolder(), "config.yml");
+            if (!configFile.exists()) {
+                //build config from defaults
+                configFile.getParentFile().mkdirs();
+                config.set("minElevation", minElevation);
+                config.set("maxElevation", maxElevation);
+                config.set("elevatorMaterial", elevatorMaterial.toString());
+                config.set("elevatorWhoosh", elevatorWhoosh.toString());
+                config.set("teleportcause", TeleportCause.UNKNOWN.name());
+                config.save(configFile);
+            }
+            config.load(configFile);
+        } catch(IOException | InvalidConfigurationException e) {
+            this.getLogger().log(Level.WARNING, "{0}Exception {1}when loading configuration file.\n{2}", new Object[]{ChatColor.RED, Color.white, e.getMessage()});
+        }
+    }
 }
